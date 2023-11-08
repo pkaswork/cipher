6902019ab16249f5520e795b67327e25d174d267
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-11-08 18:04:25 +0300

    add modal component

---
+++ crypto/src/components/Modal.jsx
@@ -0,0 +1,14 @@
+export default function Modal({ title, text, active, setActive }) {
+	return <>
+		<div className={active ? "modal-bg active" : "modal-bg"} onClick={() => setActive(false)}></div>
+		<div className={active ? "modal active" : "modal"}>
+			<div className="cross" onClick={() => setActive(false)}>
+				<span></span>
+			</div>
+			<h2 className="title">{ title }</h2>
+			<p className="text">
+			{ text }
+			</p>
+		</div>
+	</>
+}
