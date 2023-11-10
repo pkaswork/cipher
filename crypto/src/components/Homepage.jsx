8469c5de5a330381fba1ff71c8c45d8f1c41574d
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-11-10 10:04:33 +0300

    change logic for variant

--- crypto/src/components/Homepage.jsx
+++ crypto/src/components/Homepage.jsx
@@ -1,14 +1,25 @@
 /* eslint-disable react/prop-types */
-import { Link } from 'react-router-dom'
-import Headerfooter from './Headerfooter'
+import { Link, useLocation } from 'react-router-dom'
+import Header from './Header'
+import Footer from './Footer'
 
 export default function Homepage() {
-	const variant = Math.floor(1 + Math.random() * (80 + 1 - 1))
+	const location = useLocation()
+	const { from } = location.state
+	const surname = from[0]
+	const name = from[1]
+	const patronymic = from[2]
+	const variant = from[3]
 
 	return <>
 		<div className="wrapper">
 			<div className="content">
-				<Headerfooter />
+				<Header 
+					isLogin={true} 
+					surname={surname} 
+					name={name} 
+					patronymic={patronymic}
+				/>
 				<main className="main">
 					<section className="exercise">
 						<div className="container">
@@ -18,38 +29,38 @@ export default function Homepage() {
 							<div className="exercise-flex">
 								<article className="exercise-flex__elem">
 									<h2 className="title">Шифр Цезаря</h2>
-									<Link to="/exercise" state={{ from: [variant, 'cipherCaesar']}} className="text link-task">Перейти к решению</Link>
+									<Link to="/exercise" state={{ from: [surname, name, patronymic, variant, 'cipherCaesar']}} className="text link-task">Перейти к решению</Link>
 								</article>
 								<article className="exercise-flex__elem">
 									<h2 className="title">Шифр Цезаря: поиск частоты</h2>
-									<Link to="/exercise" state={{ from: [variant, 'cipherCaesarFrequency']}} className="text link-task">Перейти к решению</Link>
+									<Link to="/exercise" state={{ from: [surname, name, patronymic, variant, 'cipherCaesarFrequency']}} className="text link-task">Перейти к решению</Link>
 								</article>
 								<article className="exercise-flex__elem">
 									<h2 className="title">Шифр Гаммирования</h2>
-									<Link to="/exercise" state={{ from: [variant, 'gammingCode']}} className="text link-task">Перейти к решению</Link>
+									<Link to="/exercise" state={{ from: [surname, name, patronymic, variant, 'gammingCode']}} className="text link-task">Перейти к решению</Link>
 								</article>
 								<article className="exercise-flex__elem">
 									<h2 className="title">Шифр перестановки</h2>
-									<Link to="/exercise" state={{ from: [variant, 'permutationCipher']}} className="text link-task">Перейти к решению</Link>
+									<Link to="/exercise" state={{ from: [surname, name, patronymic, variant, 'permutationCipher']}} className="text link-task">Перейти к решению</Link>
 								</article>
 								<article className="exercise-flex__elem">
 									<h2 className="title">Шифр Трисемиуса</h2>
-									<Link to="/exercise" state={{ from: [variant, 'trisemiusCipher']}} className="text link-task">Перейти к решению</Link>
+									<Link to="/exercise" state={{ from: [surname, name, patronymic, variant, 'trisemiusCipher']}} className="text link-task">Перейти к решению</Link>
 								</article>
 								<article className="exercise-flex__elem">
 									<h2 className="title">Шифр Диффи-Хеммана</h2>
-									<Link to="/exercise" state={{ from: [variant, 'cipherDiffieHellman']}} className="text link-task">Перейти к решению</Link>
+									<Link to="/exercise" state={{ from: [surname, name, patronymic, variant, 'cipherDiffieHellman']}} className="text link-task">Перейти к решению</Link>
 								</article>
 								<article className="exercise-flex__elem">
 									<h2 className="title">RSA</h2>
-									<Link to="/exercise" state={{ from: [variant, 'rsa']}} className="text link-task">Перейти к решению</Link>
+									<Link to="/exercise" state={{ from: [surname, name, patronymic, variant, 'rsa']}} className="text link-task">Перейти к решению</Link>
 								</article>
 							</div>
 						</div>
 					</section>
 				</main>
 			</div>
-			<Headerfooter />
+			<Footer />
 		</div>
 	</>
 }
