63b5bd12a1327d648e5a1d804b4636ad03736334
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-11-12 10:33:30 +0300

    add import for image

--- crypto/src/components/Footer.jsx
+++ crypto/src/components/Footer.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
+import Image from '../assets/images/logo.svg'
 
 export default function Footer() {
 	return <>
 		<header className="footer">
 			<div className="container">
 				<a href="#" className="logo">
-					<img src='./assets/images/logo.svg' alt="logo" />
+					<img src={Image} alt="logo" />
 				</a>
 			</div>
 		</header>
