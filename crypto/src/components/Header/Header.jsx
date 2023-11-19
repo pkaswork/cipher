a5a2bd970335ce75638e87146569aba23ed9afd6
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-11-19 18:08:41 +0300

    change images

--- crypto/src/components/Header/Header.jsx
+++ crypto/src/components/Header/Header.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import './header.css';
 import Image from '../../assets/images/logo.svg'
 
 export default function Header({ isLogin, surname, name, patronymic }) {
 	return <>
 		<header className="header">
 			<div className="container">
-				{
-					(isLogin === true) ?
-					<div className="header-flex">
+				<div className="header-flex">
+					<div className="header-flex__elem">
 						<a href="#" className="logo">
 							<img src={Image} alt="logo" />
 						</a>
-						<div className="header-flex__elem">
-							<p className="text name-text">{surname} {name} {patronymic}</p>
-							<Link to="/" className="link-exit">Выйти</Link>
-						</div>
+						<Link to="/">Главная</Link>
+						<Link to="/">Теория</Link>
+						{(isLogin === true) ? <Link to="/">Задания</Link> : null}
 					</div>
-					:
-					<a href="#" className="logo">
-						<img src={Image} alt="logo" />
-					</a>
-				}
-				
+					<div className="header-flex__elem">
+						<p className="text name-text">{surname} {name} {patronymic}</p>
+						<Link to="/auth">{(isLogin === true) ? 'Выйти' : 'Войти'}</Link>
+					</div>
+				</div>
 			</div>
 		</header>
 	</>
