cf1f6587464e1cbe3dadd28669a737f0bb8fd0e7
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-11-20 20:11:22 +0300

    created theory page

---
+++ crypto/src/components/pages/Theorypage/Theorypage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Theorypage() {
+	return (
+		<>
+			<section className="theory">
+				<div className="container">
+					Теория
+				</div>
+			</section>
+		</>
+	);
+}
+
+export default Theorypage;
