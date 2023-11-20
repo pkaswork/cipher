91bed71b6d440da3065b057f7fb966e3267443ad
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-11-20 20:18:20 +0300

    moved to pages folder

---
+++ crypto/src/components/pages/Homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Homepage() {
+	return (
+		<>
+			<section className="about">
+				<div className="container">
+					О приложении
+				</div>
+			</section>
+		</>
+	);
+}
+
+export default Homepage;
