2f8ae1db633a3e893faebbec0fcc891b428d4cba
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-11-20 20:13:18 +0300

    added pages and routes for new pages

--- crypto/src/App.jsx
+++ crypto/src/App.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Variantspage from './components/Variantspage/Variantspage';
-import Homepage from './components/Homepage/Homepage';
-import Exercisepage from './components/Exercisepage/Exercisepage';
-import Loginpage from './components/Loginpage/Loginpage';
+import React, { useState } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Variantspage from './components/pages/Variantspage/Variantspage';
+import Homepage from './components/pages/Homepage/Homepage';
+import Theorypage from './components/pages/Theorypage/Theorypage';
+import Exercisepage from './components/pages/Exercisepage/Exercisepage';
+import Loginpage from './components/pages/Loginpage/Loginpage';
+
+const userData = {
+  surname: 'А',
+  name: 'А',
+  patronymic: 'А',
+  variant: '1',
+  isLogin: true
+};
 
 function App() {
+  const [user, setUser] = useState(userData);
+  const location = useLocation();
+  // const { from } = location.state;
+  console.log(location.state);
+
   return (
     <>
-      <BrowserRouter>
+      <Header isLogin={user.isLogin} surname={user.surname} name={user.name} />
+      <main className="main">
         <Routes>
-          <Route path="*" element={<Homepage />} />
+          <Route path="/" element={<Homepage />} />
           <Route path="/auth" element={<Loginpage />} />
-          <Route path="/exercises" element={<Variantspage />} />
-          <Route path="/exercise" element={<Exercisepage />} />
+          <Route path="/theory" element={<Theorypage />} />
+          <Route path="/exercises" element={<Variantspage surname={user.surname} name={user.name} patronymic={user.patronymic} variant={user.variant} />} />
+          <Route path="/exercise" element={<Exercisepage surname={user.surname} name={user.name} patronymic={user.patronymic} variant={user.variant} />} />
         </Routes>
-      </BrowserRouter>
+      </main>
     </>
   );
 }
