bffd67add57b8f04e47b70d9aa9af3de14227dde
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-11-21 16:52:54 +0300

    added styles and logic for burger menu

--- crypto/src/components/Header/Header.jsx
+++ crypto/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import './header.css';
 import Image from '../../assets/images/logo.svg'
 
 export default function Header({ isLogin, surname, name }) {
+	const [active, setActive] = useState(false)
+
 	return <>
 		<header className="header">
 			<div className="container">
@@ -23,7 +25,6 @@ export default function Header({ isLogin, surname, name }) {
 								{(isLogin === true) ? <li className="nav-list__item"><Link to="/exercises" className={(location.pathname === '/exercises') ? 'active nav-link' : 'nav-link'}>Задания</Link></li> : null}
 							</ul>
 						</nav>
-						
 					</div>
 					<div className="header-flex__elem">
 						<p className="nav-link">{surname} {name}</p>
@@ -34,11 +35,81 @@ export default function Header({ isLogin, surname, name }) {
 					<a href="#" className="logo">
 						<img src={Image} alt="logo" />
 					</a>
-					<div className="burger">
+					<div className="burger" onClick={() => setActive(true)}>
 						<span></span>
 					</div>
 				</div>
 			</div>
 		</header>
+		<div className={active ? "burger-menu-nav-container active" : "burger-menu-nav-container"}>
+			<div className="burger-menu-nav-flex">
+				<a href="#" className="logo">
+					<img src={Image} alt="logo" />
+				</a>
+				<div className="cross" onClick={() => setActive(false)}>
+					<span></span>
+				</div>
+			</div>
+			<nav className="burger-menu-nav">
+				<ul className="nav-list">
+					<li className="nav-list__item">
+						<Link 
+							to="/" 
+							className=
+							{
+								(location.pathname === '/') ? 'active nav-link' 
+								: 'nav-link'} onClick={() => setActive(false)
+							}
+						>
+							Главная
+						</Link>
+					</li>
+					<li className="nav-list__item">
+						<Link 
+							to="/theory" 
+							className=
+							{
+								(location.pathname === '/theory') ? 'active nav-link' 
+								: 'nav-link'
+							} 
+							onClick={() => setActive(false)}
+						>
+							Теория
+						</Link>
+					</li>
+					{
+						(isLogin === true) ? 
+						<li className="nav-list__item">
+							<Link 
+								to="/exercises" 
+								className=
+								{
+									(location.pathname === '/exercises') ? 'active nav-link' 
+									: 'nav-link'
+								} 
+								onClick={() => setActive(false)}
+							>
+								Задания
+							</Link>
+						</li> 
+						: null
+					}
+					<li className="nav-list__item">
+						<p className="nav-link">{surname} {name}</p>
+					</li>
+					<li className="nav-list__item">
+						<Link 
+							to="/auth" 
+							className="nav-link" 
+							onClick={() => setActive(false)}
+						>
+							{(isLogin === true) ? 'Выйти' 
+							: 'Войти'
+							}
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</div>
 	</>
 }
