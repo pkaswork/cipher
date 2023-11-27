36f39c6fd15dfc61ee11d427d5baed736a449f3b
Yulia Khavaeva <88404500+iamlorddop@users.noreply.github.com>
2023-11-27 16:01:31 +0300

    done homepage

---
+++ crypto/src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import react from 'react';
+import './footer.css';
+
+function Footer() {
+	return (
+		<>
+			<footer className="footer">
+				<div className="container">
+					<h3 className="text">
+						CIPHER©️2023
+					</h3>
+				</div>
+			</footer>
+		</>
+	);
+}
+
+export default Footer;
--- crypto/src/components/pages/Homepage/Homepage.jsx
+++ crypto/src/components/pages/Homepage/Homepage.jsx
@@ -1,13 +1,96 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import Footer from '../../Footer/Footer';
+import imageOne from '../../../assets/images/about_01.png';
+import imageTwo from '../../../assets/images/about_02.png';
+import imageThree from '../../../assets/images/about_03.png';
+import './homepage.css';
 
 function Homepage() {
+	const location = useLocation()
+
 	return (
 		<>
 			<section className="about">
 				<div className="container">
-					О приложении
+					<h1 className="title">
+						&lt;h1&gt;CIPHER&lt;/h1&gt;
+					</h1>
+					<h2 className="subtitle">
+						&lt;h2&gt;Приложение для изучения основ криптографии&lt;/h2&gt;
+					</h2>
+					<p className="caption">&lt;p&gt;Расшифруйте свои возможности&lt;/p&gt;</p>
+					<div className="about-flex">
+						<div className="about-flex__elem">
+							Четыре шифра
+						</div>
+						<div className="about-flex__elem">
+							80 вариантов
+						</div>
+						<div className="about-flex__elem">
+							Подробная теория
+						</div>
+					</div>
+					<article className="about-article">
+						<h2 className="title">Теоретические материалы</h2>
+						<div className="about-article__elem about-article__elem-theory">
+								<p className="binary-animation">1000011001111110101</p>
+								<p className="binary-animation">1000011001111110101</p>
+								<p className="binary-animation">10100100011110101</p>
+							<div className="about-article__elem-text">
+								<p className="text">
+									&lt;ul&gt;
+										<span className="about-article__elem-span">&lt;li&gt;Шифр Цезаря&lt;/li&gt;</span>
+										<span className="about-article__elem-span">&lt;li&gt;Шифр Тритемиуса&lt;/li&gt;</span>
+										<span className="about-article__elem-span">&lt;li&gt;Шифр Замены&lt;/li&gt;</span>
+										<span className="about-article__elem-span">&lt;li&gt;Алгоритм RSA&lt;/li&gt;</span>
+									&lt;/ul&gt;
+								</p>
+								<Link to="/theory" state={location.state} className="btn">Изучить</Link>
+							</div>
+							<img src={imageOne} alt="theory" />
+						</div>
+					</article>
+					<article className="about-article">
+						<h2 className="title">Задания</h2>
+						{
+							(!location.state) ?
+							<div className="about-article__elem about-article__elem-login">
+								<p className="binary-animation">10100100011110101</p>
+								<p className="binary-animation">1000011001111110101</p>
+								<p className="binary-animation">1000011001111110101</p>
+								<p className="binary-animation">10100100011110101</p>
+								<div className="about-article__elem-text">
+									<p className="text">
+										Авторизуйтесь,
+										<br />
+										чтобы получить доступ к заданиям
+									</p>
+									<Link to="/auth" state={location.state} className="btn">Войти</Link>
+								</div>
+								<img src={imageThree} alt="theory" />
+							</div>
+							:
+							<div className="about-article__elem about-article__elem-exercises">
+								<p className="binary-animation">10100100011110101</p>
+								<p className="binary-animation">1000011001111110101</p>
+								<p className="binary-animation">1000011001111110101</p>
+								<p className="binary-animation">10100100011110101</p>
+								<div className="about-article__elem-text">
+									<p className="text">
+										{location.state.from[0]} {location.state.from[1]},
+										<br />
+										готовы к решению заданий?
+									</p>
+									<Link to="/exercises" state={location.state} className="btn">Перейти</Link>
+								</div>
+								<img src={imageTwo} alt="theory" />
+							</div>
+						}
+					</article>
 				</div>
 			</section>
+			<Footer />
 		</>
 	);
 }
